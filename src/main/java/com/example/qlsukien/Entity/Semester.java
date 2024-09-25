922a9bb51df8b18fe5348bcd4c2f70f43949a50c
mottongmotdep9@gmail.com <mottongmotdep9@gmail.com>
2024-09-25 09:44:46 +0700

    a

--- src/main/java/com/example/qlsukien/Entity/Semester.java
+++ src/main/java/com/example/qlsukien/Entity/Semester.java
@@ -2,6 +2,10 @@ package com.example.qlsukien.Entity;
 
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
@@ -9,7 +13,11 @@ import java.util.Date;
 
 @Entity
 @Table(name = "semesters")
-public class Semester {
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class  Semester {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
